79e12f9c21067f1477a53e7a6e2bfceb21cb9024
Dima <dimaaa.m14@gmail.com>
2025-11-05 11:04:12 +0100

    Updated file to find name of table and then to manually plug the name to view the table with 10 rows.

--- svdb/OpenDB_files.py
+++ svdb/OpenDB_files.py
@@ -1,16 +1,14 @@
 import sqlite3
 import pandas as pd
 
-# Connect to your database
-conn = sqlite3.connect("svdb.db")
+conn = sqlite3.connect("SVDB.db")
+cursor = conn.cursor()
 
-# See what tables are inside
-tables = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", conn)
-print(tables)
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+tables = cursor.fetchall()
+print("Tables:", tables)
 
-# Once you know a table name, you can view it:
-df = pd.read_sql_query("SELECT * FROM your_table_name LIMIT 10;", conn)
+df = pd.read_sql_query("SELECT * FROM SVDB LIMIT 10;", conn)
 print(df.head())
 
-# Close when you're done
 conn.close()
