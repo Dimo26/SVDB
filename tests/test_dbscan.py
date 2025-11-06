a02a671b99988ba14afa6bd985fd5a4f607738ca
Dima <dimaaa.m14@gmail.com>
2025-11-06 13:42:13 +0100

    Updated the test to match the updated file

--- tests/test_dbscan.py
+++ tests/test_dbscan.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy
 
-from svdb.DBSCAN import main
+from svdb.export_module import DBSCAN
 
 
 class TestDBSCAN(unittest.TestCase):
@@ -11,7 +11,7 @@ class TestDBSCAN(unittest.TestCase):
         data = numpy.array([[1,1],[1,101]])
         epsilon=100
         m=2
-        result=main(data,epsilon,m)
+        result=DBSCAN.cluster(data,epsilon,m)
         assert (result[0] == -1 and result[1] == -1)
 
     #test that close points are merged
@@ -19,7 +19,7 @@ class TestDBSCAN(unittest.TestCase):
         data = numpy.array([[1,1],[1,101]])
         epsilon=200
         m=2
-        result=main(data,epsilon,m)
+        result=DBSCAN.cluster(data,epsilon,m)
         assert (result[0] == 0 and result[1] == 0)
 
     #test that small clusters smaller than m are not merged
@@ -27,5 +27,5 @@ class TestDBSCAN(unittest.TestCase):
         data = numpy.array([[1,1],[1,1],[1,101],[1,101]])
         epsilon=100
         m=3
-        result=main(data,epsilon,m)
+        result=DBSCAN.cluster(data,epsilon,m)
         assert (result[0] == -1 and result[1] == -1)
